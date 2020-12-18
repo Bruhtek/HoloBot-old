ceb99aea4052db8fa94ad81c0b389ef5de4928c0
Bruhtek <41647874+Bruhtek@users.noreply.github.com>
2020-12-18 21:21:24 +0100

    Shion version one
    Currently only outputs shion time, up to 1h precision

---
+++ commands/time.js
@@ -0,0 +1,21 @@
+exports.run = async (client, message, args, level) => {
+    let date = new Date();
+    let half = date.getHours() / 2 + (date.getMinutes() > 30 ? 0.5 : 0);
+    let realtime = date.getHours() + ":" + date.getMinutes();
+    message.channel.send(realtime + " => " + half);
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['shion'],
+    permLevel: "Bot Tester", // IMPORTANT! SET THIS EARLY!!!
+    logCommand: false
+  };
+  
+  exports.help = { // MOST IMPORTANT COMMAND PART!!!!!!!!!!!
+    name: "time",
+    category: "Hololive", 
+    description: "return the current time of the day",
+    usage: "time"
+  };
