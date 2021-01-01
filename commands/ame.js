ff92cb592a63e0442ea23b782158a545956949f4
Bruhtek <41647874+Bruhtek@users.noreply.github.com>
2021-01-01 18:45:11 +0100

    Fix commands

--- commands/ame.js
+++ commands/ame.js
@@ -4,13 +4,14 @@ exports.run = async (client, message, args, level) => {
 
     const link = "https://yt3.ggpht.com/ytc/AAUvwngENhxNHPzV4jjip28G3vswMxutvkaBhBjAMS0i=s88-c-k-c0x00ffffff-no-rj";
     const nick = "Watson Amelia";
-    message.guild.channels
+    message.guild.channels.cache
         .find(channel => channel.id === message.channel.id)
-        .createWebhook(nick, link).catch(e => client.logger.error(e))
-        .then(webhook => {
-            const wb = new WebhookClient(`${webhook.id}`,`${webhook.token}`);
-            wb.send("Thats what I did to your mom last night!");
-            wb.delete();
+        .createWebhook(nick, {
+            avatar: link}
+        ).catch(e => client.logger.error(e))
+        .then(async webhook => {
+            await webhook.send("Thats what I did to your mom last night!");
+            webhook.delete();
             message.delete();
         }).catch(e => client.logger.error(e));
 };
--- commands/time.js
+++ commands/time.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args, level) => {
     let date = new Date();
     let hour = date.getHours();
     let minutes = date.getMinutes();
-    let shion = client.emojis.get('789583909348048947');
+    let shion = client.emojis.cache.get('789583909348048947');
     if(minutes < 30) return message.channel.send(`Less than a quarter ${shion} has passed after ${hour}!`);
     else return message.channel.send(`It's less than a quarter ${shion} to ${hour+1}!`);
   };
