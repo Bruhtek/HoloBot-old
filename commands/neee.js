c5842a3a36584bde8c958d4b5f691500b78c6fa0
Bruhtek <41647874+Bruhtek@users.noreply.github.com>
2021-01-01 19:23:28 +0100

    NEEEEEEEEEEEEE

---
+++ commands/neee.js
@@ -0,0 +1,36 @@
+exports.run = async (client, message, args, level) => {
+    if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+
+        const dispatcher = connection.play('resources/audio/rushia.mp3');
+        message.channel.send("NEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+
+        dispatcher.on('start', () => {
+            //
+        });
+
+        dispatcher.on('finish', () => {
+            connection.disconnect();
+        });
+
+        // Always remember to handle errors appropriately!
+        dispatcher.on('error', console.error);
+    } else {
+        return message.reply("join a voice channel first, bruh.");
+    }
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: ['ne', 'nee', 'neee', 'neeee', 'neeeee'],
+    permLevel: "User", // IMPORTANT! SET THIS EARLY!!!
+    logCommand: false
+  };
+  
+  exports.help = { // MOST IMPORTANT COMMAND PART!!!!!!!!!!!
+    name: "neee",
+    category: "Hololive",
+    description: "NEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+    usage: "neee"
+  };
