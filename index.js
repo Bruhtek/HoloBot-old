cfd19d712bca4bc083a447fb3339acdcce0b8981
Bruhtek <41647874+Bruhtek@users.noreply.github.com>
2021-06-24 20:55:58 +0200

    Add a messanger web relayer

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.getUser = require("./config.js").getUser;
 client.logger = require("./modules/Logger");
 require("./modules/functions.js")(client);
 
-client.connector = mongoose.connect(client.uri, {useNewUrlParser: true, useUnifiedTopology: true});
+client.connector = mongoose.connect(client.uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.commands = new Enmap();
 client.aliases = new Enmap();
@@ -26,39 +26,46 @@ client.settings = client.config.defaultSettings;
 const GuildSettings = require("./schemes/settingsSchema");
 const Dashboard = require("./dashboard/dashboard");
 
-const init = async () => {
+const init = async () =>
+{
 
-  const cmdFiles = await readdirp.promise("./commands/", {fileFilter: '*.js'});
-  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
-  cmdFiles.forEach(f => {
-    if (!f.path.endsWith(".js")) return;
-    const response = client.loadCommand(f.path);
-    if (response) console.log(response);
-  });
+	const cmdFiles = await readdirp.promise("./commands/", { fileFilter: '*.js' });
+	client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+	cmdFiles.forEach(f =>
+	{
+		if (!f.path.endsWith(".js")) return;
+		const response = client.loadCommand(f.path);
+		if (response) console.log(response);
+	});
 
-  const evtFiles = await readdir("./events/");
-  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
-  evtFiles.forEach(file => {
-    const eventName = file.split(".")[0];
-    client.logger.log(`Loading Event: ${eventName}`);
-    const event = require(`./events/${file}`);
-    client.on(eventName, event.bind(null, client));
-  });
+	const evtFiles = await readdir("./events/");
+	client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+	evtFiles.forEach(file =>
+	{
+		const eventName = file.split(".")[0];
+		client.logger.log(`Loading Event: ${eventName}`);
+		const event = require(`./events/${file}`);
+		client.on(eventName, event.bind(null, client));
+	});
 
-  client.levelCache = {};
-  for (let i = 0; i < client.config.permLevels.length; i++) {
-    const thisLevel = client.config.permLevels[i];
-    client.levelCache[thisLevel.name] = thisLevel.level;
-  }
+	client.levelCache = {};
+	for (let i = 0; i < client.config.permLevels.length; i++)
+	{
+		const thisLevel = client.config.permLevels[i];
+		client.levelCache[thisLevel.name] = thisLevel.level;
+	}
 
-  keepAlive();
-  await client.login(process.env.TOKEN);
+	keepAlive();
+	await client.login(process.env.TOKEN);
 
-  const crons = await readdir("./cronjobs/");
-  client.logger.log(`Loading a total of ${crons.length} cron-jobs.`);
-  client.crons = crons;
+	const crons = await readdir("./cronjobs/");
+	client.logger.log(`Loading a total of ${crons.length} cron-jobs.`);
+	client.crons = crons;
 
-  Dashboard(client);
+	const fb = require("./modules/facebook/main");
+	fb.setup(client);
+
+	Dashboard(client);
 };
 
 init();
