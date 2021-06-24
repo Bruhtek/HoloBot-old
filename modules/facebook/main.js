d12aeaa2281e4d9a48331ba774bd0dbbd010b27a
Bruhtek <41647874+Bruhtek@users.noreply.github.com>
2021-06-24 21:13:19 +0200

    Revert "Calculate the summary of colors on profilePic, add them to embed"
    This reverts commit 6bee8b3d4d5807f147a33444306cf653a91e650b.

--- modules/facebook/main.js
+++ modules/facebook/main.js
@@ -2,8 +2,7 @@ const facebookLogin = require('ts-messenger-api').default;
 const fs = require('fs');
 const { MessageEmbed } = require("discord.js");
 const login = require("facebook-chat-api");
-const { getAverageColor } = require('fast-average-color-node');
-var request = require('request').defaults({ encoding: null });
+
 
 let fbDiscordChannelId = "857692851407552513";
 
@@ -23,33 +22,21 @@ exports.setup = async (client) =>
 			if (msg.threadId != "3022302291231344") return;
 			apiUser.getUserInfo([msg.senderId], async (err, data) =>
 			{
-				let profilePic = data[msg.senderId].thumbSrc;
-
-				request.get(profilePic, function (error, response, body)
+				const embed = new MessageEmbed()
+					.setDescription(msg.body)
+					.setColor("#0345fc")
+					.setAuthor(data[msg.senderId].name, data[msg.senderId].thumbSrc)
+					.setTimestamp()
+				// if there is an attachment, check if it's a photo, and if yes, add it to the embed
+				if (msg.attachments[0])
 				{
-					if (!error && response.statusCode == 200)
+					if (msg.attachments[0].type == 1)
 					{
-						profileBase = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
-						getAverageColor(profileBase).then(async (color) =>
-						{
-							const embed = new MessageEmbed()
-								.setDescription(msg.body)
-								.setColor(color.hex)
-								.setAuthor(data[msg.senderId].name, profilePic)
-								.setTimestamp()
-							// if there is an attachment, check if it's a photo, and if yes, add it to the embed
-							if (msg.attachments[0])
-							{
-								if (msg.attachments[0].type == 1)
-								{
-									embed.setImage(msg.attachments[0].previewLarge.uri);
-								}
-							}
-
-							await channel.send({ embed });
-						});
+						embed.setImage(msg.attachments[0].previewLarge.uri);
 					}
-				});
+				}
+
+				await channel.send({ embed });
 			});
 		})
 	});
